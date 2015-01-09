232428c7c0f9b5e0c9d5efd5bba74e32a8bc0bcc
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2015-01-09 18:17:17 +0300

    added stub for new Design

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,17 +2,20 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import views.html
 
 object Application extends Controller {
 
   // TODO : temporary section
   val Admin = Redirect(admin.routes.Administration.home)
-
+  val Stub = Ok(html.auxiliary.stub())
 
   //
   // Actions
   //
 
-  def index = Action { Admin }
+//  def index = Action { Admin }
+  def index = Action { Stub }
+
 
 }
